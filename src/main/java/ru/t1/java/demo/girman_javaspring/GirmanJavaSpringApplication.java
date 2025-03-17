d37384c704df48e26d7f1ce214f941af83e2508f
Nikita <kissesksk@gmail.com>
2025-03-17 22:42:14 +0300

    Initial commit

---
+++ src/main/java/ru/t1/java/demo/girman_javaspring/GirmanJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.java.demo.girman_javaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirmanJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirmanJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/t1/java/demo/girman_javaspring/GirmanJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1.java.demo.girman_javaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirmanJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
